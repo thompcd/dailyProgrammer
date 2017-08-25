fa2c4a632753b1ec4ab406cc860a4a084f924236
BootcampUser <BootcampUser@DESKTOP-GHP5H69>
2017-08-25 06:40:27 -0500

    Project Inception.
    Created RunChallenges as executable project, which calls created challenges
    
    Created utility projects Toolbox and ConstantsLibrary, both meant to hold statics.
    
    Challenge321E completed, take time as an int and returns in a formatted string

--- Challenges/Class1.cs
+++ Challenges/Class1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static ConstantsLibrary.Constants;
+using Toolbox;
 
 /// <summary>
 /// r/Daily Programmer Challenges
@@ -12,7 +13,6 @@ using static ConstantsLibrary.Constants;
 /// </summary>
 namespace Challenges
 {
-
     /// <summary>
     /// A talking clock takes 24 hr time &
     /// translates into words!
@@ -28,14 +28,20 @@ namespace Challenges
 
         public static string TimeToString(string value)
         {
-            var valueArray = value.ToCharArray();
-            if (valueArray.Length == 1)
+            if (value.Length > 2) return TimeConstants.Error;
+
+            //if single digit time, must start with '0'
+            if (value.Length == 1)
             {
-                //pad array left (w/ ascii zero)
+                string zero = "0";
+                value = value.Insert(0, zero);
             }
+            var valueOnes = value[1].ToString();
+            var valueTens = value[0].ToString();
+
             //if leading value is 0, can exclude all other possibilities
 
-            if (valueArray[0] == Ascii.Zero)
+            if (valueTens == "0")
             {
                 if (value == "00") return TimeConstants.Twelve;
                 if (value == "01") return TimeConstants.One;
@@ -49,21 +55,42 @@ namespace Challenges
                 if (value == "09") return TimeConstants.Nine;
             }
             //if starts with "1" is either a special teen or [0]+"teen"
-            if (valueArray[0] == 1)
+            if (valueTens == "1")
             {
                 if (value == "10") return TimeConstants.Ten;
                 if (value == "11") return TimeConstants.Eleven;
                 if (value == "12") return TimeConstants.Twelve;
                 if (value == "13") return TimeConstants.Thirteen;
+
+                //if begins with one and has not found case yet,
+                //number is a teen with standard format
             }
-            //all others are [constant]+[constant]
-            if (value == "20") return TimeConstants.Twenty;
-            if (value == "30") return TimeConstants.Thirty;
-            if (value == "40") return TimeConstants.Forty;
-            if (value == "50") return TimeConstants.Fifty;
 
-            return TimeConstants.Error;
+                StringBuilder returnValue = new StringBuilder { };
 
+                //all others are [constant]+[constant]
+                if (valueTens == "2") returnValue.Insert(0,TimeConstants.Twenty    );
+                if (valueTens == "3") returnValue.Insert(0,TimeConstants.Thirty    );
+                if (valueTens == "4") returnValue.Insert(0,TimeConstants.Forty     );
+                if (valueTens == "5") returnValue.Insert(0,TimeConstants.Fifty     );
+                // insert space between words if not a teen
+                if (valueTens != "1")
+                    returnValue.Insert(returnValue.Length, " ");
+
+                if (valueOnes == "0") returnValue.Insert(returnValue.Length, TimeConstants.Twelve) ;
+                if (valueOnes == "1") returnValue.Insert(returnValue.Length, TimeConstants.One   ) ;
+                if (valueOnes == "2") returnValue.Insert(returnValue.Length, TimeConstants.Two   ) ;
+                if (valueOnes == "3") returnValue.Insert(returnValue.Length, TimeConstants.Three ) ;
+                if (valueOnes == "4") returnValue.Insert(returnValue.Length, TimeConstants.Four  ) ;
+                if (valueOnes == "5") returnValue.Insert(returnValue.Length, TimeConstants.Five  ) ;
+                if (valueOnes == "6") returnValue.Insert(returnValue.Length, TimeConstants.Six   ) ;
+                if (valueOnes == "7") returnValue.Insert(returnValue.Length, TimeConstants.Seven ) ;
+                if (valueOnes == "8") returnValue.Insert(returnValue.Length, TimeConstants.Eight ) ;
+                if (valueOnes == "9") returnValue.Insert(returnValue.Length, TimeConstants.Nine  ) ;
+
+                if (valueTens == "1") returnValue.Insert(returnValue.Length, "teen");
+
+                return returnValue.ToString();
 
         }
     }
