250e498f9df7ba9f690303b52d6e798c26b062da
BootcampUser <BootcampUser@DESKTOP-GHP5H69>
2017-08-29 05:11:03 -0500

    Added Challenes -> Challenge_328_E Challenge_328_E currently will only print a matrix as a return and does not yet contain logic to determine if a latin square

---
+++ Challenges/Challenge_328_E.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public static class Challenge_328_E
+    {
+        public static int[,] main(int n, int[] genericArray)
+        {
+            //var squareArray = createSquareArrays(n, genericArray);
+
+            try
+            {
+                return createSquareArrays(n, genericArray);
+            }
+            catch
+            {
+                return new int[,] { };
+  
+            }
+            //// Loop over each dimension's length.
+            //for (int i = 0; i < squareArray.GetLength(1); i++)
+            //{
+            //    for (int y = 0; y < squareArray.GetLength(0); y++)
+            //    {
+            //        Console.WriteLine();
+            //    }
+            //    Console.WriteLine();
+            //}
+
+        }
+        private static int[,] createSquareArrays(int n, int[] genericArray)
+        {
+            int index = 0;
+            int[,] squareArray = new int[n,n];
+            //load single dim array into 2 dim array
+            for (int y = 0; y < n; y++)
+            {
+                for (int x = 0; x < n; x++)
+                {
+                    squareArray[x, y] = genericArray[index];
+                    index++;                }
+            }
+            //int index = 0;
+            //int sqrt = (int)Math.Sqrt(array.Length);
+            //int[,] multi = new int[sqrt, sqrt];
+            //for (int y = 0; y < sqrt; y++)
+            //{
+            //    for (int x = 0; x < sqrt; x++)
+            //    {
+            //        multi[x, y] = array[index];
+            //        index++;
+            //    }
+            //}
+            //return multi;
+            return squareArray;
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,9 +8,29 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        //Challenge_321_E//static void Main(string[] args)
+        //Challenge_321_E//{
+        //Challenge_321_E//    Console.WriteLine(Challenges.Challenge_321_E.main(DateTime.Now));
+        //Challenge_321_E//}
         static void Main(string[] args)
         {
-            Console.WriteLine(Challenges.Challenge_321_E.main(DateTime.Now));
+            var squareArray = Challenges.Challenge_328_E.main
+                (5, 
+                new int[] {1, 2, 3, 4, 5, 5, 1, 2, 3, 4, 4, 5, 1, 2, 3, 3, 4, 5, 1, 2, 2, 3, 4, 5, 1 });
+
+            //print answer as a pretty matrix
+            int rowLength = squareArray.GetLength(0);
+            int colLength = squareArray.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("{0} ", squareArray[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+            Console.ReadLine();
         }
     }
 }
